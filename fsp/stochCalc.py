9aac5f8bba7d8052295e7ae909e48ce99e2d46df
Jacob Khalili <jacobkal18@gmail.com>
2022-12-23 03:45:36 -0500

    almost done with black-shultz

--- fsp/stochCalc.py
+++ fsp/stochCalc.py
@@ -4,40 +4,47 @@ import matplotlib.pyplot as plt
 
 
 class SDE:
-    def __init__(X0 : float) -> None:
+    def __init__(self, X0 : float, lambda_t : float, N : int, Beta, Gamma) -> None:
         '''
         Define an SDE Object, which simulates an SDE of the form: 
 
-        $$X[n] = X[0] + \sum_{m=0}^{n-1} \Beta(m\lambda, X[m]) \lambda + \sum_{m=0}^{n-1} \gamma(m\lambda, X[m]) d_w[m]$$
+        $$X[n] = X[0] + \sum_{m=0}^{n-1} \beta(m\lambda, X[m]) \lambda + \sum_{m=0}^{n-1} \gamma(m\lambda, X[m]) d_w[m]$$
         :param X0: the initial value of the X, ie $X[0]$
+        :param lambda_t: the lambda in the equation above (the unit of discretized time)
+        :param N: the length of time simulated
+        :param Beta: a function that takes in $m*\lambda$, and $X[m]$, and returns a floating point value. 
+        :param Gamma: same as above.
         '''
 
+        self.X0 = X0
+        self.lambda_t = lambda_t
+        self.N = N
+        self.Beta = Beta 
+        self.Gamma = Gamma
+        self.badPathCounter = 0
+
+    def getValidPath(self):
         
+        W = np.random.standard_normal(size=self.N+1)
+        W = np.cumsum(W)*np.sqrt(self.lambda_t)
+        X = np.zeros(self.N)
 
-    def getPaths(M:int, X0:float, mu:float, sigma:float):
-        '''
-        Get sample path 
-        '''
-        mu = 1
-        n = 50
-        dt = 0.1
-        x0 = 100
-        np.random.seed(12)
+        drift = 0
+        driven = 0
 
-        sigma = np.arange(0.8, 2, 0.2)
+        X[0] = self.X0
+        for n in range(1, self.N):
+            drift =+ self.Beta(X[n-1]) * self.lambda_t
+            driven =+ self.Gamma(X[n-1]) *(W[n]-W[n-1])
+            X[n] = X[n-1] + drift + driven
 
-        x = np.exp(
-            (mu - sigma ** 2 / 2) * dt
-            + sigma * np.random.normal(0, np.sqrt(dt), size=(len(sigma), n)).T
-        )
-        x = np.vstack([np.ones(len(sigma)), x])
-        x = x0 * x.cumprod(axis=0)
+            if(X[n] < 0):
+                self.badPathCounter += 1
+                Warning("A path with Negative X0 was generated, retrying. BAD PATH #", self.badPathCounter)
+                return self.getValidPath()
 
-        plt.plot(x)
-        plt.legend(np.round(sigma, 2))
-        plt.xlabel("$t$")
-        plt.ylabel("$x$")
-        plt.title(
-            "Realizations of Geometric Brownian Motion with different variances\n $\mu=1$"
-        )
-        plt.show()
+        return X
+
+    def getValidPaths(self, M=1000):
+        for _ in range(M):
+            yield self.getValidPath()
